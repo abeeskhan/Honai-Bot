5eb3ba1bc5ab3ef9c05f1db0c4844db20ad6bc54
abeeskhan <abeeskhan@gmail.com>
2021-08-21 12:44:27 +0530

    Initial Commit

---
+++ src/main/java/com/strange/honai/HonaiApplication.java
@@ -0,0 +1,13 @@
+package com.strange.honai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HonaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/strange/honai/HonaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strange.honai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HonaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
